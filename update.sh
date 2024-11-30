a95c881c6b18b1c6af437f9203b18ad1ee254b3a
HellDove <helldove.ha@gmail.com>
2024-11-30 18:22:40 +0900

    init files

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+FULL_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FULL_PATH)
+ELF_PATH="$DIR_PATH/factorio/bin/x64/factorio"
+CURR_VERSION=""
+
+if [[ ! -f "$ELF_PATH" ]]; then
+	CURR_VERSION="Not Installed"
+else
+	CURR_VERSION="$($ELF_PATH --version | grep 'Version' | head -1 | awk '{print $2}' | grep -Po '[0-9\.]+')"
+fi
+
+CHK_VERSION="$(curl https://factorio.com/get-download/stable/headless/linux64 -s --max-redirs 0 | head -5 | tail -1 | grep -Po 'releases/[0-9\.]*_' | grep -Po '[0-9\.]+' | head -1)"
+
+echo "Current version: $CURR_VERSION"
+echo "Latest version: $CHK_VERSION"
+
+
+if [[ "${CURR_VERSION}" == "${CHK_VERSION}" ]]; then
+	echo "Already latest version."
+else
+	echo "Update start."
+	`wget -q https://factorio.com/get-download/stable/headless/linux64 -O factorio.tar.xz`
+	`tar -xvf factorio.tar.xz; rm -rf factorio.tar.xz`
+	
+	
+	UPDATE_VERSION="$($ELF_PATH --version | grep 'Version' | head -1 | awk '{print $2}' | grep -Po '[0-9\.]+')"
+	if [[ ! -L "$DIR_PATH/factorio/saves" ]]; then
+		`ln -s "$DIR_PATH/saves" "$DIR_PATH/factorio/saves"`
+	fi
+	if [[ ! -L "$DIR_PATH/factorio/data/server-settings.json" ]]; then
+		`ln -s "$DIR_PATH/settings/server-settings.json" "$DIR_PATH/factorio/data"`
+	fi
+
+	echo "Update complete: ${CURR_VERSION} -> ${UPDATE_VERSION}"
+fi
